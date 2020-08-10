32460a3c4eab5c44adfeff83e64bf4447f9c75f8
MBJ <madsj812@gmail.com>
2020-08-10 13:56:53 +0200

    clean up

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,42 +1,44 @@
 import express from 'express';
 import expressWs from 'express-ws';
 import * as ws from 'ws';
+import HashTable from './Util/HashTable';
 
 const app = expressWs(express()).app;
-
-interface HashTable<T> {
-  [key: string]: T;
-}
 
 var games: HashTable<ws[]> = {};
 
 app.ws('/game', (ws, req) => {
-  if (!req.query.ID) {
-    ws.close();
+  if (!req.query.ID || !req.query.Name) {
+    ws.close(1000, 'No Game ID');
     return;
   }
 
   let ID = req.query.ID as string;
+  let Name = req.query.Name as string;
+
+  if (Name.length < 3) {
+    ws.close();
+    return;
+  }
 
   ws.on('message', (msg: String) => {
-    console.log(`${msg}`);
     ws.send(msg);
   });
 
   ws.on('close', () => {
     RemovePlayerFromGame(ID, ws);
-    BoardCastToGame(ID, `A Player has Left`);
+    BoardCastToGame(ID, `${Name} has Left`);
   });
 
   ws.on('error', () => {
     RemovePlayerFromGame(ID, ws);
-    BoardCastToGame(ID, `A Player got disconnected`);
+    BoardCastToGame(ID, `Player got disconnected`);
   });
 
   console.log(games[ID]);
 
   AddPlayerToGame(ID, ws);
-  BoardCastToGame(ID, `A new Player has joined with game ID ${req.query.ID}`);
+  BoardCastToGame(ID, `${Name} has joined this room`);
 });
 
 function AddPlayerToGame(ID: string, socket: ws) {
